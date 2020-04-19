4ab636b5fb27567431d216743a92da75becca84c
Alexander Zenov <alexander.zenov@gmail.com>
2020-04-19 14:42:14 +0300

    Add base structure

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,20 @@
-fn main() {}
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn main_page() -> &'static str {
+  "Main page!"
+}
+
+#[get("/other")]
+fn other_page() -> &'static str {
+  "Other page!"
+}
+
+fn main() {
+  rocket::ignite()
+    .mount("/", routes![main_page, other_page])
+    .launch();
+}
