f90d37df5e93924b7275d8934033473b7e32dc9b
Alexander Zenov <alexander.zenov@gmail.com>
2020-04-20 22:13:19 +0300

    Add pages and cookies routes

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,16 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::http::Cookies;
+
 #[get("/")]
 fn main_page() -> String {
   format!("Main page!")
+}
+
+#[get("/pages")]
+fn pages() -> String {
+  format!("Pages")
 }
 
 #[get("/page/<id>")]
@@ -13,8 +20,15 @@ fn page_by_id(id: i32) -> String {
   format!("Page is {}", &id)
 }
 
+#[get("/cookies")]
+fn cookies(cookies: Cookies) -> Option<String> {
+  cookies
+    .get("message")
+    .map(|value| format!("Message: {}", value))
+}
+
 fn main() {
   rocket::ignite()
-    .mount("/", routes![main_page, page_by_id])
+    .mount("/", routes![main_page, pages, page_by_id, cookies])
     .launch();
 }
