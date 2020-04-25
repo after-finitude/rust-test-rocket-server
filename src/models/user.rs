5d0f41533a086577e874ffc57e874043cf6c10fd
Alexander Zenov <alexander.zenov@gmail.com>
2020-04-25 14:29:00 +0300

    Add method for user struct

--- src/models/user.rs
+++ src/models/user.rs
@@ -7,3 +7,20 @@ pub struct User {
   pub email: String,
   pub hash: String,
 }
+
+#[derive(Serialize)]
+pub struct UserAuth<'a> {
+  username: &'a str,
+  email: &'a str,
+  token: String,
+}
+
+impl User {
+  pub fn to_user_auth(&self, secret: &[u8]) -> UserAuth {
+    UserAuth {
+      username: &self.username,
+      email: &self.email,
+      token: "",
+    }
+  }
+}
